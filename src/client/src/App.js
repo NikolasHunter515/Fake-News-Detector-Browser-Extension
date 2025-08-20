54d9316d4168e1bdc1e421bdc40cde6966ddcc75
Nikolas Hunter <radnik2004@gmail.com>
2025-08-20 17:21:33 -0400

    Project structure established, react, nodejs added, and manifest set for browser extension.

---
+++ src/client/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Node App</h1>
+      <p>Message from server: {message}</p>
+    </div>
+  );
+}
+
+export default App;
