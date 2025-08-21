c849634809de1dcef8a62280614526b95b687c46
Nikolas Hunter <radnik2004@gmail.com>
2025-08-21 15:27:17 -0400

    Change made to background script

--- src/client/public/background.js
+++ src/client/public/background.js
@@ -1,1 +1,15 @@
 console.log('Background script loaded.');
+
+chrome.tabs.onMessage.addListener((message, sender, sendResponse) => {
+  if (message === "getActiveTabUrl") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0];
+      if (activeTab) {
+        sendResponse({ url: activeTab.url });
+      } else {
+        sendResponse({ url: "No active tab found" });
+      }
+    });
+    return true; // Important: keep the message channel open
+  }
+});
