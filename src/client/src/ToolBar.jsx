3d1909b92e418933be2a3ba238a18f9e840309c4
Nikolas Hunter <radnik2004@gmail.com>
2025-08-21 15:20:26 -0400

    Generic layout added

---
+++ src/client/src/ToolBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseChimneyUser, faGear, faCircleHalfStroke, faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
+//TODO change this layout, move the settings and light/dark to top of page.
+export default function ToolBar(){
+    return(
+        <div className="justify-content-center">
+            <nav className="navbar navbar-expand-sm bg-light fixed-bottom">
+                <div className="container-fluid">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="index.html">
+                                <FontAwesomeIcon icon={faHouseChimneyUser} />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">
+                                <FontAwesomeIcon icon={faPowerOff} />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">
+                                <FontAwesomeIcon icon={faCircleHalfStroke} />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <FontAwesomeIcon icon={faGear} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
